06858fb82dba169568e76d77caa577971283f073
= <=>
2020-05-16 11:34:48 +0000

    migrating to hiera vars

--- code/environments/test/modules/nginx/manifests/init.pp
+++ code/environments/test/modules/nginx/manifests/init.pp
@@ -2,9 +2,9 @@ class nginx (
   String  $version, 
   String  $status, 
   Boolean $enabled, 
-  String  $proxy_redirect        = 'off',
-  String  $client_max_body_size  = '10m',
-  String  $proxy_connect_timeout = '90',
+  String  $proxy_redirect,
+  String  $client_max_body_size,
+  String  $proxy_connect_timeout,
 ){
   notice ( "Install the $version of NginX, ensure it's $status, and set boot time start $enabled." )
 
@@ -27,12 +27,11 @@ class nginx (
     content => template('nginx/proxy.conf.erb'), 
   }
 
-  file {'/tmp/nginx/':
+  file { '/tmp/nginx':
     ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0444',
-    source => 'puppet:///modules/nginx/tmp_nginx',
   }
 }
 
