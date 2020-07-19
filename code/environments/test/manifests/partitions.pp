9e7b247beae56145db2cb751578e250a72eb5df1
= <=>
2020-07-19 13:13:44 +0000

    + deleted manifests

--- code/environments/test/manifests/partitions.pp
+++
@@ -1,3 +0,0 @@
-each( $facts['partitions'] ) |$disk| {
-  notice( "This host has disk with name: ${disk[0]} and size: ${disk[1]['size']}" )
-}
--- code/environments/test/manifests/system_uptime.pp
+++
@@ -1,3 +0,0 @@
-reverse_each( $facts['system_uptime'] ) |$uptime| {
-  notice( "This is the sys uptime: ${uptime[1]} ${uptime[0]}" )
-}
