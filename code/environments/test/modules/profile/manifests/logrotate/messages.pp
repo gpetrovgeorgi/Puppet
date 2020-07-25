d4ada93578ab11244cc2141d6be607a4de0a07db
= <=>
2020-07-25 15:10:44 +0000

    changing a str to a num

--- code/environments/test/modules/profile/manifests/logrotate/messages.pp
+++ code/environments/test/modules/profile/manifests/logrotate/messages.pp
@@ -5,7 +5,7 @@ class profile::logrotate::messages {
   logrotate::rule { 'var_log_messages':
     ensure        => present,
     rotate_every  => 'week',
-    rotate        => '4',
+    rotate        => 4,
     delaycompress => true,
     compress      => true,
     path          => '/var/log/messages',
